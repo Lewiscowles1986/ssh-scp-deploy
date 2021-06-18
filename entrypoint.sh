d0194868f52ee84b542b88d5704a29d966fc81ec
Lewis Cowles <lewis+github@cowles.uk>
2021-06-18 19:37:15 +0100

    Modify upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ echo -e "${INPUT_KEY}" >__TEMP_INPUT_KEY_FILE
 
 chmod 600 __TEMP_INPUT_KEY_FILE
 
-scp -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -P "${INPUT_PORT}" -r ${INPUT_SRC} "${INPUT_USER}"@"${INPUT_HOST}":"${INPUT_REMOTE}"
+GLOBIGNORE=.git scp -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -P "${INPUT_PORT}" -r ${INPUT_SRC} "${INPUT_USER}"@"${INPUT_HOST}":"${INPUT_REMOTE}"
+rm -rf __TEMP_INPUT_KEY_FILE
